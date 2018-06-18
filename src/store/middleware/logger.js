25f1c41116e5133a0004fc2f6770c072321127f8
Dejan Besic <d.besic@vegaitsourcing.rs>
2018-06-18 16:16:11 +0200

    more testing

--- src/store/middleware/logger.js
+++
@@ -1,9 +0,0 @@
-const logging =  store => next => action => {
-  console.log(`type: ${action.type}`, action);
-  const result = next(action);
-  console.log('next state', store.getState());
-  return result;
-};
-
-export default logging;
-  
