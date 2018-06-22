504f5d2ef0aedc3ca53dab56b21198b4c914f9f2
Mario <Mario@DESKTOP-N11VBT2>
2018-06-22 16:43:50 +0200

    a

---
+++ Booking/src/main/resources/static/scripts/addAdmin.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+	defaultElements();
+});
+var token;
+
+function defaultElements(){
+	var url = window.location.href;
+	var str = url.split("key=");
+	if(str.length < 2){
+		window.location.href = "../AdminLogin.html";
+		return;
+	}
+	token = str[1];
+	var title = "Add Agent";
+	document.title = title;
+	$('#header').html(title);
+}
+
+function addAgent(){
+	var data = new Object();
+	data.name = $("#name").val();
+	data.lastName = $("#lastName").val();
+	data.address = $("#address").val();
+	data.pmb = $("#pmb").val();
+	data.email = $("#email").val();
+	
+	$.ajax({
+    	url: "../admin/addAgent",
+		data: JSON.stringify(data),
+		type: "POST",
+		contentType: "application/json",
+		dataType: "json",
+		headers: {
+			Authorization :"Bearer "+token
+		},
+        success: function (data) {
+        	alert("asd");
+        },
+		error: function(xhr, ajaxOptions, thrownError){
+			console.log(thrownError);
+
+		}
+	}); 
+}
+
+function returnt(){
+	window.location.href = "../AdminHome.html?key="+token;
+}
+	
