2f2d4a8e06c9ccaf574cc842757e852428714c2a
Mario <Mario@DESKTOP-N11VBT2>
2018-06-23 23:30:01 +0200

    s

---
+++ Booking/src/main/java/com/booking/app/DTOs/ReservationDTO.java
@@ -0,0 +1,60 @@
+package com.booking.app.DTOs;
+
+import java.util.Date;
+
+public class ReservationDTO {
+
+	private Long id;
+	private String guestName;
+	private String guestUsername;
+	private String facility;
+	private Date fromDate;
+	private Date toDate;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getGuestName() {
+		return guestName;
+	}
+	public void setGuestName(String guestName) {
+		this.guestName = guestName;
+	}
+	public String getGuestUsername() {
+		return guestUsername;
+	}
+	public void setGuestUsername(String guestUsername) {
+		this.guestUsername = guestUsername;
+	}
+	public String getFacility() {
+		return facility;
+	}
+	public void setFacility(String facility) {
+		this.facility = facility;
+	}
+	public Date getFromDate() {
+		return fromDate;
+	}
+	public void setFromDate(Date fromDate) {
+		this.fromDate = fromDate;
+	}
+	public Date getToDate() {
+		return toDate;
+	}
+	public void setToDate(Date toDate) {
+		this.toDate = toDate;
+	}
+	public ReservationDTO(Long id, String guestName, String guestUsername, String facility, Date fromDate,
+			Date toDate) {
+		super();
+		this.id = id;
+		this.guestName = guestName;
+		this.guestUsername = guestUsername;
+		this.facility = facility;
+		this.fromDate = fromDate;
+		this.toDate = toDate;
+	}
+	
+}
