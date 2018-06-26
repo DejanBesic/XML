97d99eb79dea012652db8e42d4ee09a9b0a9b475
Stefan Radanovic <radanovicstefan@gmail.com>
2018-06-26 22:34:40 +0200

    ss

---
+++ Booking-agent/src/main/java/com/agent/app/model/User.java
@@ -0,0 +1,170 @@
+package com.agent.app.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Transient;
+
+@Entity
+public class User implements Serializable {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+    
+    @Column(nullable = true)
+    private String name;
+    
+    @Column(nullable = true)
+    private String lastName;
+    
+    @Column(nullable = true)
+    private String address;
+    
+    @Column(nullable = true)
+    private String pmb;
+    
+    @Column(nullable = false)
+    private String username;
+    
+    @Column(nullable = false)
+    private String password;
+    
+    @Transient
+    private String passwordConfirm;
+    
+    @Column(nullable = false)
+    private String email;
+    
+    @Column(nullable = true)
+    private String token;
+    
+    @Column(nullable = true)
+    private boolean active;
+    
+    @ManyToOne(optional = false)
+	private Role role;
+
+	
+	public User(){
+	}
+	
+	public User(String username, String email, String password, Role role) {
+		this.username = username;
+		this.email = email;
+		this.password = password;
+		this.role = role;
+	}
+	
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    
+    public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPasswordConfirm() {
+        return passwordConfirm;
+    }
+
+    public void setPasswordConfirm(String passwordConfirm) {
+        this.passwordConfirm = passwordConfirm;
+    }
+
+    
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPmb() {
+		return pmb;
+	}
+
+	public void setPmb(String pmb) {
+		this.pmb = pmb;
+	}
+	
+	
+}
---
+++ Booking-agent/src/main/java/com/agent/app/repository/UserRepository.java
@@ -0,0 +1,32 @@
+package com.agent.app.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.agent.app.model.User;
+
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+    User findByEmail(String email);
+
+    User findByUsernameOrEmail(String username, String email);
+
+    List<User> findByIdIn(List<Long> userIds);
+    
+    User findById(Long id);
+
+    User findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+    
+    @Query("SELECT r FROM User r where r.active = false") 
+    List<User> findAllInactive();
+}
